ae6c390f520851d4438a69b14f4fcc58fdce88ec
Ketty Lezama <ketty.lezama9@gmail.com>
2020-09-07 10:21:42 -0400

    WIP, added footer & initial styling to Learn More

--- app/javascript/react/components/LearnMorePage.js
+++ app/javascript/react/components/LearnMorePage.js
@@ -3,9 +3,21 @@ import React from 'react';
 const LearnMorePage = (props) => {
 
   return (
-    <div id="learn-more-container">
-      <h1>Hi, I am the Learn MorePage!</h1>
-    </div>
+    <section id="learn-more-page-hero" className="hero is-fullheight">
+      <div className="hero-body">
+        <div id="learn-more-container" className="container is-fluid columns">
+          <div id="learn-more-content" className="column is-left">
+            <p>
+              
+            </p>
+          </div>
+
+          <div id="learn-more-picture" className="column is-right">
+
+          </div>
+        </div>
+      </div>
+    </section>
   )
 };
 
