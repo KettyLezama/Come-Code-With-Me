afe9985ded06104dc2de5dc228a0a9e0f3ed380d
Ketty Lezama <ketty.lezama9@gmail.com>
2020-09-07 11:03:08 -0400

    WIP, made messages dependent in connection model

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -2,7 +2,7 @@ class Connection < ApplicationRecord
   belongs_to :user
   belongs_to :interest, class_name: "User"
 
-  has_many :messages
+  has_many :messages, dependent: :delete_all
 
   def self.create_reciprocal_for_ids(user_id, interest_id)
     user_connection = Connection.create(user_id: user_id, interest_id: interest_id)
@@ -13,6 +13,7 @@ class Connection < ApplicationRecord
   def self.destroy_reciprocal_for_ids(user_id, interest_id)
     connection1 = Connection.find_by(user_id: user_id, interest_id: interest_id)
     connection2 = Connection.find_by(user_id: interest_id, interest_id: user_id)
+    
     connection1.destroy
     connection2.destroy
   end
